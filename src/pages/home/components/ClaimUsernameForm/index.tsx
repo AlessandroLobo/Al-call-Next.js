419c741fea76f78eb4b5f9e3650512422c06e6a2
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-02-22 16:16:38 -0300

    add page register

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,67 @@
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
+import { useRouter } from 'next/router'
+
+// Define a schema to validate the form data
+const ClaimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'The username must have at least 3 letters.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'The username can only contain letters and hyphens.',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+// Infer the type of the form data from the schema
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(ClaimUsernameFormSchema),
+  })
+
+  const router = useRouter()
+
+  // Handle the form submission
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    const { username } = data
+
+    // Redirect to the registration page with the reserved username
+    router.push(`/register?username=${username}`)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="alsystem.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit" disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {/* Display an error message if the username is invalid, otherwise display a prompt */}
+          {errors.username
+            ? errors.username.message
+            : 'Enter the desired username'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,80 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react' // Importing UI components from Ignite UI
+import { ArrowRight } from 'phosphor-react' // Importing an icon from Phosphor
+import { Container, Form, FormError, Header } from './styles' // Importing styled components
+import { useForm } from 'react-hook-form' // Importing useForm hook from react-hook-form
+import { z } from 'zod' // Importing zod for data validation
+import { zodResolver } from '@hookform/resolvers/zod' // Importing zodResolver from hookform resolvers
+
+// Defining the schema for the registration form using zod
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Username must be at least 3 characters.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'Username can only contain letters and hyphens.',
+    })
+    .transform((username) => username.toLowerCase()),
+  name: z.string().min(3, { message: 'Name must be at least 3 characters.' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema> // Defining the type of the form data using the schema
+
+export default function Register() {
+  // Using the useForm hook to handle the form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema), // Using zodResolver to validate the form data
+  })
+
+  // Function to handle form submission
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data) // Logging the form data to the console
+  }
+
+  // Rendering the registration form
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Welcome to Al Call</Heading>{' '}
+        {/* Using a Heading component from Ignite UI */}
+        <Text>
+          Please provide some information to create your profile. You can edit
+          this information later.
+        </Text>
+        <MultiStep size={4} currentStep={1} />{' '}
+        {/* Using a MultiStep component from Ignite UI */}
+      </Header>
+
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+        <label>
+          <Text size="sm">Username</Text>{' '}
+          {/* Using a Text component from Ignite UI */}
+          <TextInput
+            prefix="alsystem.com/"
+            placeholder="your-username"
+            {...register('username')} // Using the register function to register the username input field
+          />
+          {errors.username && (
+            <FormError size="sm">{errors.username?.message}</FormError> // Displaying an error message if the username input is invalid
+          )}
+        </label>
+        <label>
+          <Text size="sm">Full Name</Text>{' '}
+          {/* Using a Text component from Ignite UI */}
+          <TextInput placeholder="Your name" {...register('name')} />
+          the register function to register the name input field
+          {errors.name && (
+            <FormError size="sm">{errors.name?.message}</FormError> // Displaying an error message if the name input is invalid
+          )}
+        </label>
+        <Button type="submit" disabled={isSubmitting}>
+          Next Step
+          <ArrowRight /> {/* Using an icon from Phosphor */}
+        </Button>
+      </Form>
+    </Container>
+  )
+}
