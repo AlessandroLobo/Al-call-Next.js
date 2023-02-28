f4f1815687d9b27490e84afe67c495a99ead9cc3
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-02-28 10:44:20 -0300

    add calendar

---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+import { CalendarStep } from './CalendarStep'
+// import { ConfirmStep } from './ConfirmStep'
+
+export function ScheduleForm() {
+  // const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  return <CalendarStep />
+}
